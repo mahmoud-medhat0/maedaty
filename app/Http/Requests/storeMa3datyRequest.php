d60adeda2f6256f80c69963ce6818bef1eb0c18f
Mahmoud Medhat <hhetlar120@gmail.com>
2023-03-23 16:25:32 +0200

    add request

---
+++ app/Http/Requests/storeMa3datyRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class storeMa3datyRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+                'title' => "required",
+                'address' => "required||max:200",
+                'lat' => "required",
+                'lng' => "required",
+                'for' => "required",
+                'maedaType' => "required",
+                'government_id' => "required",
+                'city_id' => "required",
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'title.required' => "لا يمكن تركت اسم المائدة فارغ",
+            'address.required' => "لا يمكن ترك عنوان المائدة فارغ",
+            'address.max' => "العنوان يجب ان يكون اقل من 200 حرف",
+            'lat.required' => "يجب اختيار مكان على الخريطة أو تحديد خط العرض",
+            'lng.required' => "يجب اختيار مكان على الخريطة أو تحديد خط الطول",
+            'for.required' => "يجب تحديد لمن تكون المائدة",
+            'maedaType.required' => "يجب تحديد نوع المائدة",
+            'government_id.required' => "يجب اختيار المحافظة",
+            'city_id.required' => "يجب تحديد المركز",
+        ];
+    }
+}
