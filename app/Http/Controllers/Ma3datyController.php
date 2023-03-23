75d2fd14c6d03dff50b02e634e44e514feb3c9af
hamo332 <31453260+hamo332@users.noreply.github.com>
2023-03-23 15:27:28 +0200

    gfhfgh

--- app/Http/Controllers/Ma3datyController.php
+++ app/Http/Controllers/Ma3datyController.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\storeCardsRequest;
+use App\Http\Requests\storeMa3datyRequest;
 
 class Ma3datyController extends Controller
 {
